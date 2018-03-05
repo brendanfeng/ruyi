0b4aae8992156ce73cf6d8fe9f5910d3be31d5f1
Brendan <brendan.feng@gmail.com>
2018-03-04 19:41:03 -0500

    npm setup

---
+++ frontend/ruyi.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementbyId('root');
+  ReactDOM.render(<h1>RUYI</h1>, root);
+});
