bf272b2d90d4f5846d4cabd3fd3bb4b8cb161e8a
Brendan <brendan.feng@gmail.com>
2018-03-06 15:25:23 -0500

    Complete Store, start Components

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,40 @@
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const signup = newUser => dispatch => {
+  return APIUtil.signup(newUser).then(user => {
+    return dispatch(receiveCurrentUser(user));
+  }, err => {
+    return dispatch(receiveErrors(err.responseJSON));
+  });
+};
+
+export const login = loggedUser => dispatch => {
+  return APIUtil.login(loggedUser).then(user => {
+    return dispatch(receiveCurrentUser(user));
+  }, err => {
+    return dispatch(receiveErrors(err.responseJSON));
+  });
+};
+
+export const logout = () => dispatch => {
+  return APIUtil.logout().then(user => {
+    return dispatch(receiveCurrentUser(null));
+  }, err => {
+    return dispatch(receiveErrors(err.responseJSON));
+  });
+};
--- frontend/ruyi.jsx
+++ frontend/ruyi.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
+
+// START_TESTING!!!!
+let store = configureStore();
+window.getState = store.getState;
+window.dispatch = store.dispatch;
+// TESTING_END
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
