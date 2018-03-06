afce41277dd76f2bb841a350a759a93d9b9dd3aa
Brendan <brendan.feng@gmail.com>
2018-03-06 15:02:03 -0500

    complete backend auth and session_api_util

--- frontend/ruyi.jsx
+++ frontend/ruyi.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementbyId('root');
+  const root = document.getElementById('root');
   ReactDOM.render(<h1>RUYI</h1>, root);
 });
