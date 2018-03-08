34f30b962cafc137f644079663da7f68348355d5
Brendan <brendan.feng@gmail.com>
2018-03-08 00:38:35 -0500

    changed class to className

--- frontend/components/header/form_button.jsx
+++ frontend/components/header/form_button.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const FormButton = ({loggedIn, logout}) => (
   loggedIn ? (
     <button className="header-user-menu" onClick={ () => logout() }>
-      <i class="material-icons">mood</i>
+      <i className="material-icons">mood</i>
     </button>
     ) : (
     <Link className="header-link" to="/login">Sign In</Link>
