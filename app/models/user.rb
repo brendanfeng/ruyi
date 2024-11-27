1582cccda8f38b1aa63b882358f6cd91b8477d94
Brendan Feng <brendan.feng@gmail.com>
2024-11-27 12:12:28 -0500

    user.rb change + render yaml start command

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,7 @@ class User < ApplicationRecord
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
-    if user && user.is_password?(password)
-      user
-    else
-      nil
-    end
+    user if user && user.is_password?(password)
   end
 
   def self.generate_session_token
@@ -22,8 +18,8 @@ class User < ApplicationRecord
 
   def reset_session_token!
     self.session_token = User.generate_session_token
-    self.save!
-    self.session_token
+    save!
+    session_token
   end
 
   def is_password?(password)
@@ -36,10 +32,10 @@ class User < ApplicationRecord
   end
 
   has_many :created_projects,
-    primary_key: :id,
-    foreign_key: :creator_id,
-    class_name: "Projects",
-    dependent: :destroy
+           primary_key: :id,
+           foreign_key: :creator_id,
+           class_name: 'Project',
+           dependent: :destroy
 
   private
 
