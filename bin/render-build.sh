c19f9b10f25afe0f73a07093569a1535a7ac699c
Brendan Feng <brendan.feng@gmail.com>
2024-11-27 12:54:26 -0500

    change build command to include seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -10,4 +10,4 @@ bundle exec rails assets:clean
 # perform database migrations in the build command.
 # Uncomment the following line:
 
-bundle exec rails db:migrate
+bundle exec rails db:{migrate,seed}
